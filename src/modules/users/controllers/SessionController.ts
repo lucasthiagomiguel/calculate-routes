a468863f8fa49c5706b2e1b4cc1b1eed25d5bc47
lucas pasquali <lucas.souza@pasqualisolution.com.br>
2024-01-18 17:51:05 -0300

    fixing code login

--- src/modules/users/controllers/SessionController.ts
+++ src/modules/users/controllers/SessionController.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from 'express'
 import { repositoryUsers } from '../repositories/UsersRepository'
-import { findUserEmailQuery,createUserQuery } from '@shared/querys';
-import AppError from "../../../shared/erros/AppErrors"
+import { findUserEmailQuery } from '@shared/querys';
 import { compare } from 'bcryptjs'
 import { sign } from 'jsonwebtoken'
-import authConfig from '../../../config/auth';
+import authConfig from '@config/auth';
 import { Users } from '../entities/Users'
 
 interface IRequest {
@@ -19,44 +18,39 @@ interface IRequest {
 
 export  default class SessionController {
 	public async create(req: Request, res: Response) {
-        const { email,password } = req.body;
+    const { email,password } = req.body;
 
-        try {
-            const users = await findUserEmailQuery(repositoryUsers,email)
+    try {
+      const users = await findUserEmailQuery(repositoryUsers,email);
 
-            if (!users) {
-                throw new AppError('Incorrect email/password combination.', 401);
-              }
-            // console.log(users,authConfig.jwt.secret);
+      if (users.rowCount == 0) {
+        return res.status(400).json( {status:400,menssage:'Incorrect email/password combination.'} );
+      }
 
-            const passwordConfirmed = await compare(password, users.password);
+      const passwordConfirmed = await compare(password, users.rows[0].password);
 
-            if (!passwordConfirmed) {
-                throw new AppError('Incorrect email/password combination.', 401);
-            }
-
-
-            const token = sign({ id: users.id }, authConfig.jwt.secret, {
-                expiresIn: authConfig.jwt.expiresIn,
-              });
-
-              console.log(token)
+      if (!passwordConfirmed) {
+        return res.status(400).json( {status:400,menssage:'Incorrect email/password combination.'} );
+      }
 
 
-              return res.json( {users,
-                token })
+      const token = sign({ id: users.rows[0].id }, authConfig.jwt.secret, {
+        expiresIn: authConfig.jwt.expiresIn,
+      });
+      return res.json( {users:{
+        id:users.rows[0].id,
+        name:users.rows[0].name,
+        email:users.rows[0].email,
+        status:users.rows[0].status,
+        created:users.rows[0].created,
+        updated:users.rows[0].updated
+      },
+      token })
 
 		} catch (error) {
 			console.log(error)
 
 			return res.status(500).json( error )
 		}
-
-
-    }
-
-
-
+  }
 }
-
-
